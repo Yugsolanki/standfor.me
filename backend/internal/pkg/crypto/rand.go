8514a71cc5d66865e5d9b84c970e3480d206b455
Yug Solanki <yugsolanki15@gmail.com>
2026-04-02 01:28:08 +0530

    feat: Add EncodeBase64 function and GenerateRandomBytes

--- backend/internal/pkg/crypto/rand.go
+++ backend/internal/pkg/crypto/rand.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"crypto/rand"
+	"encoding/base64"
 	"encoding/binary"
 )
 
@@ -17,4 +18,17 @@ func CryptoFloat64() (float64, error) {
 
 	// Convert to float64 in range [0,1)
 	return float64(u) / (1 << 53), nil
+}
+
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func EncodeBase64(b []byte) string {
+	return base64.URLEncoding.EncodeToString(b)
 }
